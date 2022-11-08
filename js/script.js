cb4994a4151a520ee86ea749515544ae226ab071
Harrison Fackrell <20hfackrell@gmail.com>
2022-11-07 22:33:54 -0700

    Externalize scripts and styling; standardize script.js to be d3-semantic

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 Promise.all([d3.csv('./data/vgsales.csv')]).then( data =>
-    {
-        let vgsales = data[0];
-        //let wordNoForceData = data[1];
+{
+  let vgsales = data[0];
+  //let wordNoForceData = data[1];
+  //console.log("d", wordNoForceData);
+  // let groupedData = d3.groups(wordPositionsData, d => {
+  //     return d.category;
+  // })
+  // console.log("groupedData:",groupedData.keys())
+  // grouped it using array
+  // let table = new Table(wordPositionsData, 'loadOnce', colorScale);
+  // table.drawTable();
+  let platformGrouped = d3.group(vgsales, d => d.Platform)
+  // console.log(platformGrouped)
+  let bar = new barChart(vgsales,platformGrouped);
+  // console.log(vgsales)
+});
 
-        //console.log("d", wordNoForceData);
-        // let groupedData = d3.groups(wordPositionsData, d => {
-        //     return d.category;
-        // })
-        // console.log("groupedData:",groupedData.keys())
+d3.selectAll(".arrow").on("click",  e => {
+  let arrowParent = e.target.parentElement.parentElement;
+  arrowParent.classList.toggle("showMenu");
+});
 
-        // grouped it using array
-
-        // let table = new Table(wordPositionsData, 'loadOnce', colorScale);
-        // table.drawTable();
-
-        let platformGrouped = d3.group(vgsales, d => d.Platform)
-        // console.log(platformGrouped)
-        let bar = new barChart(vgsales,platformGrouped);
-
-        // console.log(vgsales)
-
-        
-    });
+d3.select(".bx-menu").on("click", e => {
+  let sidebar = d3.select(".sidebar");
+  sidebar.classed("close", sidebar.classed("close") ? false : true);
+});
